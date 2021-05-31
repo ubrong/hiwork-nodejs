c8409aaff25b41539bac0ea67623c8c2d2418e6e
youi0017 <chujiaye@foxmail.com>
2021-05-31 15:22:49 +0800

    优化取值方式

--- system/Base.js
+++ system/Base.js
@@ -1,9 +1,15 @@
 const path = require('path');
 
+// 测试：
+// console.log(__dirname)
+// console.log(path.join(__dirname, 'abc'));
+// console.log(path.resolve())
+	
+
 // 中间件公用数据：stat
 const state = {
 	// 根目录
-	docRoot: path.join(__dirname, '../'),
+	docRoot: path.resolve(),//默认最后无/,
 	
 }
 
@@ -13,7 +19,6 @@ module.exports = async (ctx, next)=>{
 	for(let k in state){
 		ctx.state[k] = state[k];
 	}
-	
 	
 	await next();
 	// next();
