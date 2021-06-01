a458442a5fa1184e469302c0e44f84ab3443be52
youi0017 <chujiaye@foxmail.com>
2021-06-01 15:34:24 +0800

    加入读取配置文件config.js

--- system/Base.js
+++ system/Base.js
@@ -4,14 +4,15 @@ const path = require('path');
 // console.log(__dirname)
 // console.log(path.join(__dirname, 'abc'));
 // console.log(path.resolve())
-	
 
-// 中间件公用数据：stat
-const state = {
-	// 根目录
-	docRoot: path.resolve(),//默认最后无/,
-	
-}
+// 1.配置级数据(这些数据将做为中间件数据传递)
+const state = require('../config.js');
+
+// 2.计算级数据
+state.docRoot = path.resolve(),//默认最后无/,
+
+// console.log(state);
+
 
 // 导出闭包
 module.exports = async (ctx, next)=>{
@@ -21,8 +22,6 @@ module.exports = async (ctx, next)=>{
 	}
 	
 	await next();
-	// next();
-	
 }
 
 
