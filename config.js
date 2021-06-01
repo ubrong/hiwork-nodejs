cf8c576124f0320dc5abe45231706b71f02f29d0
youi0017 <chujiaye@foxmail.com>
2021-06-01 15:31:48 +0800

    环境配置文件

---
+++ config.js
@@ -0,0 +1,12 @@
+/* 
+	配置说明：
+	modeType： develop|product
+		develop: 所有logger均被console和error.log记录
+		product: 所有logger均被error.log记录
+ 
+ */
+
+
+module.exports={
+	modeType: 'develop',//模式: {开发develop, 产品：product,}
+}
